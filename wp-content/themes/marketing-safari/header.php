bb360fedd3e1aa5c82266059ab47b1fdf2d57217
arminisa95 <arminisa@outlook.com>
2026-04-15 21:27:32 +0200

    pusher47

--- wp-content/themes/marketing-safari/header.php
+++ wp-content/themes/marketing-safari/header.php
@@ -3,6 +3,9 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate">
+  <meta http-equiv="Pragma" content="no-cache">
+  <meta http-equiv="Expires" content="0">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
