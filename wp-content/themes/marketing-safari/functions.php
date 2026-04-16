a9bb4941e098760f07391ebc02991e9c6a24ad70
arminisa95 <arminisa@outlook.com>
2026-04-16 23:24:52 +0200

    final push2

--- wp-content/themes/marketing-safari/functions.php
+++ wp-content/themes/marketing-safari/functions.php
@@ -38,23 +38,13 @@ function ms_image_url($file_name = '') {
 
 function ms_content($key, $default = '') {
     if (function_exists('get_field')) {
-        $acf_value = get_field($key, 'option');
+        $acf_value = get_field($key);
         if ($acf_value !== null && $acf_value !== false && $acf_value !== '') {
             return $acf_value;
         }
     }
 
     return get_theme_mod($key, $default);
-}
-
-if (function_exists('acf_add_options_page')) {
-    acf_add_options_page([
-        'page_title' => 'Website Inhalte',
-        'menu_title' => 'Website Inhalte',
-        'menu_slug'  => 'ms-options',
-        'capability' => 'edit_posts',
-        'redirect'   => false,
-    ]);
 }
 
 add_action('wp_enqueue_scripts', function() {
